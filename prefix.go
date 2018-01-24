f3fb7f4b1467d249f6b86ceeaffde87032204fd3
Andy Watson <andy.watson@useremedy.com>
2018-01-24 14:28:49 -0500

    only 1 return from uuid.NewV4

--- prefix.go
+++ prefix.go
@@ -23,7 +23,7 @@ func (u PrefixUUID) String() string {
 
 // GenerateUUID generates a UUID with the given prefix.
 func GenerateUUID(prefix string) PrefixUUID {
-	uid, _ := uuid.NewV4()
+	uid := uuid.NewV4()
 	id := PrefixUUID{
 		Prefix: prefix,
 		UUID:   uid,
